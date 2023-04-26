142c69f89ee0de38eb4497bcc88c21279122fdff
SaaHeal <sahilam18.sa@gmail.com>
2023-04-26 13:06:04 +0530

    initial-commit

---
+++ 11.py
@@ -0,0 +1,56 @@
+Python 3.9.2 (tags/v3.9.2:1a79785, Feb 19 2021, 13:44:55) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 100+8
+108
+>>> 116-8
+108
+>>> 2*2
+4
+>>> 2 * 2
+4
+>>> 4/2
+2.0
+>>> 100/3
+33.333333333333336
+>>> 100//3
+33
+>>> 2**3
+8
+>>> 2*2*2
+8
+>>> 10**10
+10000000000
+>>> 100**2
+10000
+>>> 2***2
+SyntaxError: invalid syntax
+>>> sahil
+Traceback (most recent call last):
+  File "<pyshell#12>", line 1, in <module>
+    sahil
+NameError: name 'sahil' is not defined
+>>> 'sahil'
+'sahil'
+>>> print(sahil)
+Traceback (most recent call last):
+  File "<pyshell#14>", line 1, in <module>
+    print(sahil)
+NameError: name 'sahil' is not defined
+>>> print('Virat')
+Virat
+>>> print("virat's century is the best thing to watch")
+virat's century is the best thing to watch
+>>> print("Virat's century is the best thing to watch")
+Virat's century is the best thing to watch
+>>> print("Virat is a cricketer\nAnd a he's best in the world")
+Virat is a cricketer
+And a he's best in the world
+>>> print("\rVirat Kohli is Vamika's father and Anushka's husband")
+Virat Kohli is Vamika's father and Anushka's husband
+>>> print("\rVirat Kohli is Vamika's father and Anushka's husband")
+Virat Kohli is Vamika's father and Anushka's husband
+>>> print("Virat Kohli is Vamika's father and Anushka's husband")
+Virat Kohli is Vamika's father and Anushka's husband
+>>> print("\Virat Kohli is Vamika's father and Anushka's husband")
+\Virat Kohli is Vamika's father and Anushka's husband
+>>> 
---
+++ 33.py
@@ -0,0 +1,4 @@
+a = "7489"
+a = int(a)
+print(type(a))
+print(a + 4)
